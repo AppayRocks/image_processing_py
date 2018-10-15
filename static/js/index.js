634fde004ee9f05f5d255c6ba634da3165f08f77
Ubuntu <ubuntu@ip-172-31-34-188.us-east-2.compute.internal>
2018-10-15 17:54:00 +0000

    added canny and sobel image processing module improved GUI for uploading image

---
+++ static/js/index.js
@@ -0,0 +1,17 @@
+$(function(){
+  var viewModel = {};
+  viewModel.fileData = ko.observable({
+    dataURL: ko.observable(),
+  });
+  viewModel.onClear = function(fileData){
+    if(confirm('Are you sure?')){
+      fileData.clear && fileData.clear();
+    }                            
+  };                    
+  viewModel.debug = function(){
+    window.viewModel = viewModel;
+    console.log(ko.toJSON(viewModel));
+    debugger; 
+  };
+  ko.applyBindings(viewModel);
+});
